4c49e7a67a0a61eec6b4294926b2274b6415c47b
AlejoVLZ <avelasquez.5536@unimar.edu.ve>
2022-06-29 19:31:38 -0400

    se ha cambiado varias configuraciones esteticas de l pagina, ademas de crear la seccion del ususario

---
+++ script.js
@@ -0,0 +1,19 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
+
+var user = 'Alejandro';
+
+var usactive = document.getElementById('usname')
+usactive.innerHTML = user;
+
+var usinfo = document.getElementById('user-name')
+usinfo.innerHTML = user;
